3061bcb1df3d34703d121dcdb10abcb3a0a467f6
ropotinsviat <144438933+ropotinsviat@users.noreply.github.com>
2024-03-09 10:04:04 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const $api = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://checkers-server.onrender.com/api",
 });
 
 export default class Store {
